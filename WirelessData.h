6e34d7fed774d9408b929fb048b4bc5558e2acf9
analogMensch <105582811+analogMensch@users.noreply.github.com>
2022-12-09 17:12:36 +0100

    Update WirelessData.h

--- WirelessData.h
+++ WirelessData.h
@@ -4,7 +4,7 @@
 #define _WIRELESSDATA_H
 
 const char* ssid = "yourSSID";  //SSID
-const char* password = "yourSassword";  //password
+const char* password = "yourPassword";  //password
 const char* ntpServer = "yourTimeServer";  //time server (router or pool.ntp.org)
     
 #endif  // _WIRELESSDATA_H
